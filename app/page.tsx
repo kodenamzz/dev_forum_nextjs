7570167f34bf65c90e9f1c28f8879ac8af65002e
kodenamzz <kode4028@gmail.com>
2024-07-19 17:43:32 +0700

    add some main routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold"> abdkode nextjs, tailwind here we go!!!</h1>
-      <h2 className="h2-bold"> abdkode nextjs, tailwind here we go!!!</h2>
-      <h3 className="h3-bold"> abdkode nextjs, tailwind here we go!!!</h3>
-    </div>
-  );
-}
