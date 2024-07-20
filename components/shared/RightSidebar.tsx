d9e93e4e6a3d34a67b1819868f5297c7df879d71
kodenamzz <kode4028@gmail.com>
2024-07-20 17:47:41 +0700

    left and right sidebar

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import Link from "next/link";
+import RenderTag from "./RenderTag";
+
+const RightSidebar = () => {
+  const hotQuestions = [
+    {
+      _id: 1,
+      title:
+        "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+    },
+    { _id: 2, title: "Is it only me or the font is bolder than necessary?" },
+    { _id: 3, title: "Redux Toolkit Not Updating State as Expected" },
+    { _id: 4, title: "Can I get the course for free?" },
+    { _id: 5, title: "Async/Await Function Not Handling Errors Properly" },
+  ];
+
+  const popularTags = [
+    { _id: 1, name: "javascript", totalQuestions: 5 },
+    { _id: 2, name: "reactjs", totalQuestions: 6 },
+    { _id: 3, name: "nextjs", totalQuestions: 4 },
+    { _id: 4, name: "nodejs", totalQuestions: 7 },
+    { _id: 5, name: "typescript", totalQuestions: 5 },
+  ];
+  return (
+    <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900 ">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map((question) => (
+            <Link
+              key={question._id}
+              href={`/questions/${question._id}`}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900 ">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
